56f64e1f01d7587b23480bf90e6015d8d162b2ce
zhaolab205 <128121392+zhaolab205@users.noreply.github.com>
2024-05-22 19:12:11 +0800

    Update Other_datasets_validation.m

--- PSSES_code2024/Validation/Other_datasets_validation.m
+++ PSSES_code2024/Validation/Other_datasets_validation.m
@@ -1,42 +1,96 @@
-%% V1re-ex vs sep
-clear,clc
-load('Validation\Validation_Think.mat');
-PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Data\rGM_mask(2mm).nii');
-FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');
-NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');
-reaction1 = double(Validation_Think.dat'*PSSEaS.dat);
-reaction2 = double(Validation_Think.dat'*FE_P.dat);
-reaction3 = double(Validation_Think.dat'*NS_P.dat);
+%% Dis-Ex vs in
+clear,clc%
+load('Con_Plc\Con_dataset_Ex_In.mat');
+PSSEvS = fmri_data('Pattern\PSSEvS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Re_P = fmri_data('Validation\rdpsp_rejection_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+SIIPS1 = fmri_data('Validation\rnonnoc_v11_4_137subjmap_weighted_mean.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Hot_P = fmri_data('Validation\rdpsp_hot_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+% PINES = fmri_data('Validation\Rating_Weights_LOSO_2.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+reaction1 = double(Con_dataset_Ex_In.dat'*PSSEaS.dat);reaction2 = double(Con_dataset_Ex_In.dat'*PSSEvS.dat);
+reaction3 = double(Con_dataset_Ex_In.dat'*FE_P.dat);
+reaction4 = double(Con_dataset_Ex_In.dat'*NS_P.dat);reaction5 = double(Con_dataset_Ex_In.dat'*Re_P.dat);
+reaction6 = double(Con_dataset_Ex_In.dat'*Hot_P.dat);reaction7 = double(Con_dataset_Ex_In.dat'*SIIPS1.dat);
 figure
-ROC1 = roc_plot(reaction1,logical(Validation_Think.Y < 0), 'color', [107 190 114]/255);
+ROC1 = roc_plot(reaction1,logical(Con_dataset_Ex_In.Y > 0), 'color', [144 201 231]/255);%, 'plotmethod', 'observed'
 hold on
-ROC2 = roc_plot(reaction2,logical(Validation_Think.Y > 0), 'color', [55 183 158]/255);
-ROC3 = roc_plot(reaction3,logical(Validation_Think.Y > 0), 'color', [69 76 160]/255);
-%% V2fe-ex vs sep
+ROC2 = roc_plot(reaction2,logical(Con_dataset_Ex_In.Y > 0), 'color', [33 158 188]/255);
+ROC3 = roc_plot(reaction3,logical(Con_dataset_Ex_In.Y < 0), 'color', [19 103 131]/255);
+ROC4 = roc_plot(reaction4, logical(Con_dataset_Ex_In.Y < 0), 'color', [2 48 74]/255);%legend('Rating 5');
+ROC5 = roc_plot(reaction5,logical(Con_dataset_Ex_In.Y > 0), 'color', [1 53 101]/255);
+ROC6 = roc_plot(reaction6,logical(Con_dataset_Ex_In.Y < 0), 'color', [255 195 0]/255);
+ROC7 = roc_plot(reaction7,logical(Con_dataset_Ex_In.Y < 0), 'color', [251 132 2]/255);
+%% Rep-Ex vs in
 clear,clc
-load('Validation\vicarious_pain_dataset\affective_vicarious_pain_clean.mat');
-PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Validation\mask.nii');
-FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Validation\mask.nii');
-NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Validation\mask.nii');
-reaction1 = double(affective_vicarious_pain.dat'*PSSEaS.dat);
-reaction2 = double(affective_vicarious_pain.dat'*FE_P.dat);
-reaction3 = double(affective_vicarious_pain.dat'*NS_P.dat);
+load('Con_Plc\Plc_dataset_Ex_In.mat');%Dis-Ex vs Other
+PSSEvS = fmri_data('Pattern\PSSEvS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Re_P = fmri_data('Validation\rdpsp_rejection_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+SIIPS1 = fmri_data('Validation\rnonnoc_v11_4_137subjmap_weighted_mean.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Hot_P = fmri_data('Validation\rdpsp_hot_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+
+reaction1 = double(Plc_dataset_Ex_In.dat'*PSSEaS.dat);reaction2 = double(Plc_dataset_Ex_In.dat'*PSSEvS.dat);
+reaction3 = double(Plc_dataset_Ex_In.dat'*FE_P.dat);
+reaction4 = double(Plc_dataset_Ex_In.dat'*NS_P.dat);reaction5 = double(Plc_dataset_Ex_In.dat'*Re_P.dat);
+reaction6 = double(Plc_dataset_Ex_In.dat'*Hot_P.dat);reaction7 = double(Plc_dataset_Ex_In.dat'*SIIPS1.dat);
 figure
-ROC1 = roc_plot(reaction1,logical(affective_vicarious_pain.Y < 0), 'color', [107 190 114]/255);
+ROC1 = roc_plot(reaction1,logical(Plc_dataset_Ex_In.Y > 0), 'color', [144 201 231]/255);%, 'plotmethod', 'observed'
 hold on
-ROC2 = roc_plot(reaction2,logical(affective_vicarious_pain.Y > 0), 'color', [55 183 158]/255);
-ROC3 = roc_plot(reaction3,logical(affective_vicarious_pain.Y > 0), 'color', [69 76 160]/255);
-%% V2ns-ex vs sep
+ROC2 = roc_plot(reaction2,logical(Plc_dataset_Ex_In.Y > 0), 'color', [33 158 188]/255);
+ROC3 = roc_plot(reaction3,logical(Plc_dataset_Ex_In.Y < 0), 'color', [19 103 131]/255);
+ROC4 = roc_plot(reaction4, logical(Plc_dataset_Ex_In.Y < 0), 'color', [2 48 74]/255);%legend('Rating 5');
+ROC5 = roc_plot(reaction5,logical(Plc_dataset_Ex_In.Y > 0), 'color', [1 53 101]/255);
+ROC6 = roc_plot(reaction6,logical(Plc_dataset_Ex_In.Y < 0), 'color', [255 195 0]/255);
+ROC7 = roc_plot(reaction7,logical(Plc_dataset_Ex_In.Y < 0), 'color', [251 132 2]/255);
+%% dis-sep vs com
 clear,clc
-load('Validation\vicarious_pain_dataset\physical_vicarious_pain_clean.mat');
-PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Validation\mask.nii');
-FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Validation\mask.nii');
-NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Validation\mask.nii');
-reaction1 = double(physical_vicarious_pain.dat'*PSSEaS.dat);
-reaction2 = double(physical_vicarious_pain.dat'*FE_P.dat);
-reaction3 = double(physical_vicarious_pain.dat'*NS_P.dat);
+load('Con_Plc\Con_dataset_Sep_Com.mat');%Dis-Ex vs Other
+PSSEvS = fmri_data('Pattern\PSSEvS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Re_P = fmri_data('Validation\rdpsp_rejection_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+SIIPS1 = fmri_data('Validation\rnonnoc_v11_4_137subjmap_weighted_mean.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Hot_P = fmri_data('Validation\rdpsp_hot_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+
+reaction1 = double(Con_dataset_Sep_Com.dat'*PSSEaS.dat);reaction2 = double(Con_dataset_Sep_Com.dat'*PSSEvS.dat);
+reaction3 = double(Con_dataset_Sep_Com.dat'*FE_P.dat);
+reaction4 = double(Con_dataset_Sep_Com.dat'*NS_P.dat);reaction5 = double(Con_dataset_Sep_Com.dat'*Re_P.dat);
+reaction6 = double(Con_dataset_Sep_Com.dat'*Hot_P.dat);reaction7 = double(Con_dataset_Sep_Com.dat'*SIIPS1.dat);
 figure
-ROC1 = roc_plot(reaction1,logical(physical_vicarious_pain.Y < 0), 'color', [107 190 114]/255);
+ROC1 = roc_plot(reaction1,logical(Con_dataset_Sep_Com.Y > 0), 'color', [144 201 231]/255);%, 'plotmethod', 'observed'
 hold on
-ROC2 = roc_plot(reaction2,logical(physical_vicarious_pain.Y > 0), 'color', [55 183 158]/255);
-ROC3 = roc_plot(reaction3,logical(physical_vicarious_pain.Y > 0), 'color', [69 76 160]/255);
+ROC2 = roc_plot(reaction2,logical(Con_dataset_Sep_Com.Y < 0), 'color', [33 158 188]/255);
+ROC3 = roc_plot(reaction3,logical(Con_dataset_Sep_Com.Y < 0), 'color', [19 103 131]/255);
+ROC4 = roc_plot(reaction4, logical(Con_dataset_Sep_Com.Y < 0), 'color', [2 48 74]/255);%legend('Rating 5');
+ROC5 = roc_plot(reaction5,logical(Con_dataset_Sep_Com.Y > 0), 'color', [1 53 101]/255);
+ROC6 = roc_plot(reaction6,logical(Con_dataset_Sep_Com.Y < 0), 'color', [255 195 0]/255);
+ROC7 = roc_plot(reaction7,logical(Con_dataset_Sep_Com.Y < 0), 'color', [251 132 2]/255);
+%% rep-sep vs com
+clear,clc
+load('Con_Plc\Plc_dataset_Sep_Com.mat');%Dis-Ex vs Other
+PSSEvS = fmri_data('Pattern\PSSEvS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+PSSEaS = fmri_data('Pattern\PSSEaS.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+FE_P = fmri_data('Validation\FE_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+NS_P = fmri_data('Validation\NS_vicarious_pain_pattern_unthresholded.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Re_P = fmri_data('Validation\rdpsp_rejection_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+SIIPS1 = fmri_data('Validation\rnonnoc_v11_4_137subjmap_weighted_mean.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+Hot_P = fmri_data('Validation\rdpsp_hot_vs_others_weights_final.nii', 'Data\rGM_mask(2mm).nii');%_FDR_05
+
+reaction1 = double(Plc_dataset_Sep_Com.dat'*PSSEaS.dat);reaction2 = double(Plc_dataset_Sep_Com.dat'*PSSEvS.dat);
+reaction3 = double(Plc_dataset_Sep_Com.dat'*FE_P.dat);
+reaction4 = double(Plc_dataset_Sep_Com.dat'*NS_P.dat);reaction5 = double(Plc_dataset_Sep_Com.dat'*Re_P.dat);
+reaction6 = double(Plc_dataset_Sep_Com.dat'*Hot_P.dat);reaction7 = double(Plc_dataset_Sep_Com.dat'*SIIPS1.dat);
+figure
+ROC1 = roc_plot(reaction1,logical(Plc_dataset_Sep_Com.Y > 0), 'color', [144 201 231]/255);%, 'plotmethod', 'observed'
+hold on
+ROC2 = roc_plot(reaction2,logical(Plc_dataset_Sep_Com.Y < 0), 'color', [33 158 188]/255);
+ROC3 = roc_plot(reaction3,logical(Plc_dataset_Sep_Com.Y < 0), 'color', [19 103 131]/255);
+ROC4 = roc_plot(reaction4, logical(Plc_dataset_Sep_Com.Y < 0), 'color', [2 48 74]/255);%legend('Rating 5');
+ROC5 = roc_plot(reaction5,logical(Plc_dataset_Sep_Com.Y > 0), 'color', [1 53 101]/255);
+ROC6 = roc_plot(reaction6,logical(Plc_dataset_Sep_Com.Y < 0), 'color', [255 195 0]/255);
+ROC7 = roc_plot(reaction7,logical(Plc_dataset_Sep_Com.Y > 0), 'color', [251 132 2]/255);
